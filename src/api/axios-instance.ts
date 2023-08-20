39c5a7f85a458efb31b78c5b66db75ef078f6f9a
Altmertrick <altmertricks@gmail.com>
2023-08-20 12:06:32 +0300

    changed api base url

--- src/api/axios-instance.ts
+++ src/api/axios-instance.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
-
+// http://localhost:5000/api
 export const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://task-manager-api-ia8l.onrender.com/api',
   withCredentials: true,
 });
